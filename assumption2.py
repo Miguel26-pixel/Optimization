25d9b05df664fd9cdf9e6cb22e8f514682270902
Miguel Amorim <migamorim2012@gmail.com>
2023-10-13 16:03:49 +0100

    Code fix to assumption2

--- assumption2.py
+++ assumption2.py
@@ -4,9 +4,10 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import scipy as sp
+import random
 
 def fi(xi, ap, am, z_list, i, j):
-    return 2 * (ap**2) * sum(xi - z_i_j for z_i_j in z_list[:i]) + 2 * (am**2) * sum(xi - z_i_j for z_i_j in z_list[j:])
+    return 2 * (ap**2) * sum(xi - z_i_j for z_i_j in z_list[:j]) + 2 * (am**2) * sum(xi - z_i_j for z_i_j in z_list[j+1:])
 
 def get_intensities_and_weights(G, node):
     intensities = []
@@ -77,8 +78,8 @@ for i in range(height):
     for j in range(width):
         if neighbor_mask[i, j] == 1:
             G.add_node((i, j), intensity=gray[i, j])
-            for dx in [-5,-4,-3, -2, -1, 0, 1, 2, 3,4,5]:
-                for dy in [-5,-4,-3, -2, -1, 0, 1, 2, 3,4,5]:
+            for dx in [-7,-6,-5,-4,-3, -2, -1, 0, 1, 2, 3,4,5,6,7]:
+                for dy in [-7,-6,-5,-4,-3, -2, -1, 0, 1, 2, 3,4,5,6,7]:
                     ni, nj = i + dx, j + dy
                     if 0 <= ni < height and 0 <= nj < width and neighbor_mask[ni,nj] == 1:
                         maxi = max(abs(dx),abs(dy))
@@ -88,9 +89,13 @@ for i in range(height):
 new_image = gray.copy()
 for node in G.nodes:
     z_list = get_intensities_and_weights(G,node)
+    z_list = sorted(z_list)
     i,j = node
 
-    result = root_scalar(fi, args=(int(ap), int(am), z_list, i, j), bracket=[-255, 255])
+    random_neg = random.randint(-3000, -1)
+    random_pos = random.randint(1, 3000)
+
+    result = root_scalar(fi, args=(random_pos, random_neg, z_list, i, j), bracket=[-255, 255])
 
     if result.converged:
         print("Zero:", result.root)
